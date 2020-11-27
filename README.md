f29fccfa912f61e41b2be4266dce40a5f8c1696a
tbradford8 <tbradford8@gatech.edu>
2020-11-27 00:09:11 -0800

    updated web site with two pie charts

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # QoL
 Uses the FHIR api to calculate the current Quality of Life of a person. 
 
-Still a work in progress
+1. install nodeJS
+1. in the terminal: `npm install`
+1. in the terminal: `npm start`
+1. in the browser: localhost:3000
+
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,57 @@
 const express = require('express');
+const QoL = require('./libs/QoLCalc').processQoL;
 const router = express.Router();
 
-let data = [
-    ['QoL', 'Quality of Life Score'],
-    ['severe problems', 8],
-    ['Moderate problems', 2],
-    ['Mild problems', 4],
-    ['Overall QoL', 8],
-    ['Number of Problems', 2]
-]
+QoL().then(function (response) {
+    console.log(response);
+});
 
-let options = {'title':'Quality of Life Score', 'width':550, 'height':400}
+let options = {'title': 'Quality of Life Score', 'width': 550, 'height': 400}
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', {title: 'QoL', pie: data, options: options});
-});
+    let chart1 = [
+        ['QoL', 'Quality of Life Score'],
+        ['Health Impact', 8],
+        ['Quality of Life', 2],
+    ]
 
+
+    QoL().then((response) => {
+        console.log("The Response went through -> ", response)
+
+        let chart1 = [
+            ['QoL', 'Quality of Life Score'],
+            ['Health Impact', 100 - response.endScore],
+            ['Quality of Life', response.endScore],
+        ]
+
+        let chart2 = [
+            ['QoL', 'Explanation of QoL scoring'],
+        ]
+
+        if (response.hasOwnProperty("impact")) {
+            for (let i in response.impact) {
+                if (response.impact.hasOwnProperty(i)) {
+                    const {value, desc} = response.impact[i];
+                    chart2.push([desc, value])
+                }
+            }
+        }
+
+        res.render('index', {
+            title: 'QoL',
+            title2: 'Factor weights for score',
+            pie: chart1,
+            options: options,
+            pie2: chart2,
+            options2: options,
+            response: response
+        });
+    })
+
+
+});
 
 
 module.exports = router;
